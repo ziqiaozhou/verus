cd92ae59bd7f45cce6ce690ef0db223b25306bdb
Chanhee Cho <channy1413@gmail.com>
2022-06-28 17:27:09 -0400

    add example

--- source/rust_verify/example/debug_expand.rs
+++ source/rust_verify/example/debug_expand.rs
@@ -74,6 +74,7 @@ spec fn is_good_option(opt: Option<int>) -> bool
 {
   match opt {
     Option::Some(x) => is_good_integer_2(x),
+//                     ^^^^^^^^^^^^^^^^^^^^
     Option::None => true,
   }
 }
@@ -82,6 +83,42 @@ proof fn test_expansion() {
   let x = Option::Some(5);
   let y = Option::Some(4);
   assert(is_good_option(x));
+//^^^^^^ ^^^^^^^^^^^^^^^^^
 }
+
+// example4: 3-level failure
+#[derive(PartialEq, Eq)] 
+pub enum Message {
+    Quit(bool),
+    Move { x: i32, y: i32 },
+    Write(bool),
+}
+
+spec fn is_good_integer_3(x: int) -> bool 
+{
+    x >= 0 && x != 5
+//  ^^^^^^^
+}
+
+spec fn is_good_message(msg:Message) -> bool {
+    match msg {
+        Message::Quit(b) => b,
+        Message::Move{x, y} => is_good_integer_3( (x as int)  - (y as int)),
+//                             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        Message::Write(b) => b,
+    }
+}
+
+spec fn is_good(msg: Message) -> bool {
+  is_good_message(msg)
+//^^^^^^^^^^^^^^^^^^^^
+}
+
+proof fn test_expansion_multiple_call() {
+  let x = Message::Move{x: 5, y:6};
+  assert(is_good(x));
+//^^^^^^ ^^^^^^^^^^
+}
+
 
 }
