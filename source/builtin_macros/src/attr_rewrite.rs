66cd444e2084e29f05110696e2bc0feb0bdccc36
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2024-10-28 11:33:56 -0700

    [attr_rewrite] Support const function using non-const spec call via closure

--- source/builtin_macros/src/attr_rewrite.rs
+++ source/builtin_macros/src/attr_rewrite.rs
@@ -214,11 +214,11 @@ fn insert_spec_call(any_fn: &mut dyn AnyAttrBlock, call: &str, verus_expr: Token
     let fname = Ident::new(call, verus_expr.span());
     let tokens: TokenStream =
         syntax::rewrite_expr(EraseGhost::Keep, true, verus_expr.into()).into();
-    any_fn.block_mut().unwrap().stmts.insert(
-        0,
-        parse2(quote! { #[verus::internal(const_header_wrapper)]||{::builtin::#fname(#tokens);};})
-            .unwrap(),
-    );
+    any_fn
+        .block_mut()
+        .unwrap()
+        .stmts
+        .insert(0, parse2(quote! { #[verus::internal(const_header_wrapper)]||{::builtin::#fname(#tokens);};}).unwrap());
 }
 
 pub fn rewrite_verus_attribute(
@@ -298,7 +298,7 @@ pub fn rewrite(
 pub fn proof_rewrite(erase: EraseGhost, input: TokenStream) -> proc_macro::TokenStream {
     if erase.keep() {
         let block: TokenStream =
-            syntax::proof_block(erase, quote_spanned!(input.span() => {#input}).into()).into();
+            syntax::proof_block(erase, quote_spanned!(input.span() => {#input;}).into()).into();
         quote! {
             #[verus::internal(proof_block)]
             {
